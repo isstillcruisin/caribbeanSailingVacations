151785b52f6797ff5d1258344f96784038774722
Michael Arick <michaelarick@gmail.com>
2019-09-12 09:48:43 -0700

    Rather than embed the sign-in form, just redirect to it.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,11 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 import SignIn from "./SignIn";
+import { Redirect } from "react-router-dom";
+
 
 const Home = () => {
-  return (
-    <div
-      className="center-align"
-      style={{ marginTop: "10rem", textAlign: "center", fontSize: "3rem" }}
-    >
-      <Fade bottom cascade>
-        <SignIn />
-      </Fade>
-    </div>
-  );
+  return <Redirect to={{ pathname: "/sign-in" }} />;
 };
 
 export default Home;
