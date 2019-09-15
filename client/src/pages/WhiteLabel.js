f74b124f8d12e3756244992daa8ab313649f53af
Michael Arick <michaelarick@gmail.com>
2019-09-15 14:40:04 -0700

    When the white label hasn't confirmed, make it so it doesn't work.

--- client/src/pages/WhiteLabel.js
+++ client/src/pages/WhiteLabel.js
@@ -3,6 +3,7 @@ import API from "../utils/API";
 import Fade from "react-reveal/Fade";
 import AllBoats from "./AllBoats";
 import Alert from '../components/Alert';
+import Loader from '../components/Loader';
 
 class WhiteLabel extends Component {
   state = {};
@@ -17,16 +18,27 @@ class WhiteLabel extends Component {
   }
 
   showWhiteLabel = () => {
-    return this.state.whiteLabel ? (
-      <div>
-        <h3>A WhiteLabel Website For Yacht Charters</h3>
-        <div>NAME: {this.state.whiteLabel.name}</div>
-        <div>EMAIL: {this.state.whiteLabel._travelAgent.email}</div>
-        <div><AllBoats whiteLabel={this.state.whiteLabel.name}/></div>
-      </div>
-    ) : (
-      <div>loading..</div>
-    );
+    if (this.state.whiteLabel) {
+      if (this.state.whiteLabel.isConfirmed) {
+        return (
+          <div>
+            <h3>A WhiteLabel Website For Yacht Charters</h3>
+            <div>NAME: {this.state.whiteLabel.name}</div>
+            <div>EMAIL: {this.state.whiteLabel._travelAgent.email}</div>
+            <div><AllBoats whiteLabel={this.state.whiteLabel.name}/></div>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            This WhiteLabel has been built, but hasn't been confirmed yet. For now, please peruse the yachts:
+            <AllBoats />
+          </div>
+        )
+      }
+    } else {
+      return <Loader />;
+    }
   };
 
   render() {
