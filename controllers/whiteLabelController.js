64f4d590afe761a89a940d83ad137c9e9c5a9b12
Michael Arick <michaelarick@gmail.com>
2019-09-15 16:22:09 -0700

    controller methods needed for the white label dashboard

--- controllers/whiteLabelController.js
+++ controllers/whiteLabelController.js
@@ -8,7 +8,8 @@ module.exports = {
     const whiteLabel = {
       _id: req.body._id,
       name: req.body.whiteLabelName,
-      _travelAgent: jwt.decode(req.body.token, keys.sessionSecret).userid
+      _travelAgent: jwt.decode(req.body.token, keys.sessionSecret).userid,
+      isConfirmed: false,
     };
     db.WhiteLabel.create(whiteLabel)
       .then(dbWhiteLabel => res.json(dbWhiteLabel))
@@ -27,11 +28,18 @@ module.exports = {
       return res.status(422).json(err)
     });
   },
-  // update: function(req, res) {
-  //   db.Boat.findOneAndUpdate({ _id: req.params.id }, req.body)
-  //     .then(dbBoat => res.json(dbBoat))
-  //     .catch(err => res.status(422).json(err));
-  // },
+
+  findAll: function(req, res) {
+    db.WhiteLabel.find(req.query)
+      .then(dbWhiteLabel => res.json(dbWhiteLabel))
+      .catch(err => res.status(422).json(err));
+  },
+  
+  update: function(req, res) {
+    db.WhiteLabel.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbWhiteLabel => res.json(dbWhiteLabel))
+      .catch(err => res.status(422).json(err));
+  },
   // remove: function(req, res) {
   //   db.Boat.findById({ _id: req.params.id })
   //     .then(dbBoat => dbBoat.remove())
