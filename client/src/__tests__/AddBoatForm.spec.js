7b4b8677edcb2a4ea4607bf271f33c3cfae3b5ea
Michael Arick <michaelarick@gmail.com>
2019-09-29 11:48:41 -0700

    Ensuring this form is properly tested.

--- client/src/__tests__/AddBoatForm.spec.js
+++ client/src/__tests__/AddBoatForm.spec.js
@@ -4,7 +4,14 @@ import AddBoatForm from '../components/AddBoatForm';
 
 describe("AddBoatForm component", () => {
   test("Matches the snapshot", () => {
-    const form = create(<AddBoatForm />);
+    const form = create(<AddBoatForm 
+      boatName='test boat'
+      crewBio='test bio'
+      maxPassengers='10'
+      year='1999'
+      manufacture='2000 feet test manufacture'
+      imgs={[]}
+    />);
     expect(form.toJSON()).toMatchSnapshot();
   });
 });
