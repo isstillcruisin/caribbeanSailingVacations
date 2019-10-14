db9db62e0ef1e6792ac331262719add4c4dd40d1
Michael Arick <michaelarick@gmail.com>
2019-10-14 11:38:28 -0700

    Updated NavBar with a bit less redundancy and a drop-down where settings link can go

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import ls from "local-storage";
 import { LinkContainer } from 'react-router-bootstrap'
 import { withRouter } from "react-router";
@@ -50,61 +51,68 @@ class IncompleteHeader extends React.Component {
 
   render() {
     if (this.state.currentUser && this.state.currentUser.isAdmin) {
-      return <Navbar bg="light" variant="light">
-              <Navbar.Brand href="#home">Charter Assistant - Admin</Navbar.Brand>
-              <Nav className="mr-auto">
-                <LinkContainer to='/'>
-                  <Nav.Link>Home</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/boats'>
-                  <Nav.Link>Yachts</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/all-white-labels'>
-                  <Nav.Link>All White Labels</Nav.Link>
-                </LinkContainer>
-              </Nav>
-              <Nav pullright={1}>
-                <LinkContainer to='/sign-out'>
-                  <Nav.Link>Sign Out</Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar>
+      return (
+        <Navbar bg="light" variant="light">
+          <LinkContainer to='/'>
+            <Navbar.Brand>Charter Assistant - Admin</Navbar.Brand>
+          </LinkContainer>
+          <Nav className="mr-auto">
+            <LinkContainer to='/boats'>
+              <Nav.Link>Yachts</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/all-white-labels'>
+              <Nav.Link>White Labels</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav pullright={1}>
+            <NavDropdown title={`${this.state.currentUser.firstName} ${this.state.currentUser.lastName}`} id="basic-nav-dropdown">
+              <LinkContainer to='/sign-out'>
+                <NavDropdown.Item>Sign Out</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>        
+          </Nav>
+        </Navbar>
+      )
     } else if (this.state.currentUser) {
-      return <Navbar bg="light" variant="light">
-              <Navbar.Brand href="#home">{`Charter Assistant - Logged In as ${this.state.currentUser.firstName}`}</Navbar.Brand>
-              <Nav className="mr-auto">
-                <LinkContainer to='/'>
-                  <Nav.Link>Home</Nav.Link>
-                </LinkContainer>
-                 <LinkContainer to='/my-white-labels'>
-                  <Nav.Link>White Labels</Nav.Link>
-                </LinkContainer>
-              </Nav>
-              <Nav pullright={1}>
-                <LinkContainer to='/sign-out'>
-                  <Nav.Link>Sign Out</Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar>
+      return (
+        <Navbar bg="light" variant="light">
+          <LinkContainer to='/'>
+            <Navbar.Brand>Charter Assistant</Navbar.Brand>
+          </LinkContainer>
+          <Nav className="mr-auto">
+            <LinkContainer to='/my-white-labels'>
+              <Nav.Link>White Labels</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav pullright={1}>
+            <NavDropdown title={`${this.state.currentUser.firstName} ${this.state.currentUser.lastName}`} id="basic-nav-dropdown">
+              <LinkContainer to='/sign-out'>
+                <NavDropdown.Item>Sign Out</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>        
+          </Nav>
+        </Navbar>
+      )
     } else if (this.state.noToolbar) {
       return ''
     } else {
-      return <Navbar bg="light" variant="light">
-              <Navbar.Brand href="#home">{'Charter Assistant'}</Navbar.Brand>
-              <Nav className="mr-auto">
-                <LinkContainer to='/'>
-                  <Nav.Link>Home</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/sign-up'>
-                  <Nav.Link>Sign Up</Nav.Link>
-                </LinkContainer>
-              </Nav>
-              <Nav pullright={1}>
-                <LinkContainer to='/sign-in'>
-                  <Nav.Link>Sign In</Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar>
+      return (
+        <Navbar bg="light" variant="light">
+          <LinkContainer to='/'>
+            <Navbar.Brand>Charter Assistant</Navbar.Brand>
+          </LinkContainer>
+          <Nav className="mr-auto">
+            <LinkContainer to='/sign-up'>
+              <Nav.Link>Sign Up</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav pullright={1}>
+            <LinkContainer to='/sign-in'>
+              <Nav.Link>Sign In</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+      )
     }
   }
 }
