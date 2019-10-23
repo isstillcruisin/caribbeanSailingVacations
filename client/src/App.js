d88aac7d5d7fdff10b0a51e18d2202c5398d90a8
Michael Arick <michaelarick@gmail.com>
2019-10-23 12:31:50 -0700

    Try to move these fonts into the head.

--- client/src/App.js
+++ client/src/App.js
@@ -110,11 +110,7 @@ class App extends Component {
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
-        />
-        
-        <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Signika&display=swap" rel="stylesheet" />
-        <link href="//db.onlinewebfonts.com/c/39a2c7f346d5cfae7045aeb2fb50d9ad?family=LuloCleanW01-OneBold" rel="stylesheet" type="text/css" />
+        /> 
         <Suspense
           fallback={
             <Background>
