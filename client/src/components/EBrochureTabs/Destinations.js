eb8147d8a98cde3067478eef1514642eb5a8614f
Michael Arick <michaelarick@gmail.com>
2019-10-28 16:50:58 -0700

    Ensure theset images have alt-text.

--- client/src/components/EBrochureTabs/Destinations.js
+++ client/src/components/EBrochureTabs/Destinations.js
@@ -10,24 +10,42 @@ const Destinations = props => (
     <i>Whether you are looking for a relaxing vacation spent on the beautiful white sand beaches of the BVI or an adventure vacation spent diving and hiking the Caribbean Islands, we have just the ting' for you. We offer trips in the USVI and BVI, as well as seasonal trips to St. Maarten, St. Barths, Antigua Puerto Rico St. Vincent & The Grenadines.</i> 
     <Row>
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/diogo-tavares-PFZTiiJnjag-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/diogo-tavares-PFZTiiJnjag-unsplash'
+          alt='biking near ocean'
+        />
       </Col>
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/eunice-c-QstAba7N2ZY-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/eunice-c-QstAba7N2ZY-unsplash'
+          alt='swimming pool near ocean'
+        />
       </Col>
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/alice-donovan-rouse-9MzCd76xLGk-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/alice-donovan-rouse-9MzCd76xLGk-unsplash' 
+          alt='delicious picnic'
+        />
       </Col>
     </Row>
     <Row>  
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/todd-desantis-tszmwrc8sJI-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/todd-desantis-tszmwrc8sJI-unsplash' 
+          alt='boardwalk to beach'
+        />
       </Col>
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/blake-cheek-tsNNCycr5MA-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/blake-cheek-tsNNCycr5MA-unsplash' 
+          alt='beach silhoutte'
+        />
       </Col>
       <Col xs={4} className='destination'>
-        <img src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/khadeeja-yasser-0E3BW7mF4Rg-unsplash' />
+        <img 
+          src='https://res.cloudinary.com/dui3yyhou/image/upload/w_400,h_300,c_fill/v1563737195/khadeeja-yasser-0E3BW7mF4Rg-unsplash' 
+          alt='walking on beach'
+        />
       </Col>
     </Row>  
   </Container>
