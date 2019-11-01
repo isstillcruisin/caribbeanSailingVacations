ad58a1ae2da2aefb08429ec325ffb6e9ffce0b7b
Michael Arick <michaelarick@gmail.com>
2019-10-31 18:03:12 -0700

    Splitting out a separate BoatDetailDisplay component

---
+++ client/src/components/BoatDetailDisplay/BoatDetailDisplay.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Carousel from 'react-bootstrap/Carousel'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+
+const BoatImage = styled.img`
+  max-height: 30rem;
+  object-fit: fill;
+`
+
+
+class BoatDetailDisplay extends Component {
+  renderImages = images => {
+    return images.map((image, i) => {
+      return (
+        <Carousel.Item key={`${i}`}>
+          <BoatImage src={image} alt=""></BoatImage>
+        </Carousel.Item>
+      )
+    })
+  };
+
+  render() {
+    return (
+      <Card className='bg-lightgreen' style={{width: '600px'}}>
+        <Card.Header>
+          <h3>Yacht: <i>{this.props.boat.boatName}</i></h3>
+        </Card.Header>
+        <Card.Body>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  Images
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Carousel style={{width: '540px'}}>
+                    {this.renderImages(this.props.boat.imgs)}
+                  </Carousel>  
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                  Specification
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <Row>
+                    <Col xs={3}>Price Per Week</Col>
+                    <Col>{`$${Number(this.props.boat.pricePerWeek).toLocaleString()}`}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}>Max Passengers</Col>
+                    <Col>{this.props.boat.maxPassengers}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}>Year</Col>
+                    <Col>{this.props.boat.year}</Col>
+                  </Row>
+                  <Row>
+                    <Col xs={3}>Manufacture</Col>
+                    <Col>{this.props.boat.manufacture}</Col>
+                  </Row>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  Crew Biography
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>{this.props.boat.crewBio}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>            
+         
+        </Card.Body>
+      </Card>
+    )
+  }
+}
+
+BoatDetailDisplay.propTypes = {
+  yacht: PropTypes.object
+}
+
+export default BoatDetailDisplay
--- client/src/pages/BoatDetail.js
+++ client/src/pages/BoatDetail.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react'
 import API from '../utils/API'
 import Loader from '../components/Loader'
-import styled from 'styled-components'
-import Carousel from 'react-bootstrap/Carousel'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import BoatDetailDisplay from '../components/BoatDetailDisplay'
 import PropTypes from 'prop-types'
-
-const BoatImage = styled.img`
-  max-height: 30rem;
-  object-fit: fill;
-`
+import Container from 'react-bootstrap/Container'
 
 class BoatDetail extends Component {
   state = {};
@@ -28,80 +17,10 @@ class BoatDetail extends Component {
     })
   }
 
-  renderImages = images => {
-    return images.map((image, i) => {
-      return (
-        <Carousel.Item key={`${i}`}>
-          <BoatImage src={image} alt=""></BoatImage>
-        </Carousel.Item>
-      )
-    })
-  };
-
   showBoat = () => {
     return this.state.boat ? (
       <Container>
-        <Card className='bg-lightgreen'>
-          <Card.Header>
-            <h3>Yacht: <i>{this.state.boat.boatName}</i></h3>
-          </Card.Header>
-          <Card.Body>
-            <Accordion defaultActiveKey="0">
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    Images
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <Carousel style={{width: '540px'}}>
-                      {this.renderImages(this.state.boat.imgs)}
-                    </Carousel>  
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                    Specification
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="2">
-                  <Card.Body>
-                    <Row>
-                      <Col xs={3}>Price Per Week</Col>
-                      <Col>{`$${Number(this.state.boat.pricePerWeek).toLocaleString()}`}</Col>
-                    </Row>
-                    <Row>
-                      <Col xs={3}>Max Passengers</Col>
-                      <Col>{this.state.boat.maxPassengers}</Col>
-                    </Row>
-                    <Row>
-                      <Col xs={3}>Year</Col>
-                      <Col>{this.state.boat.year}</Col>
-                    </Row>
-                    <Row>
-                      <Col xs={3}>Manufacture</Col>
-                      <Col>{this.state.boat.manufacture}</Col>
-                    </Row>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    Crew Biography
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>{this.state.boat.crewBio}</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>            
-           
-          </Card.Body>
-        </Card>
+        <BoatDetailDisplay boat={this.state.boat} />
       </Container>
     ) : (
       <Loader/>
